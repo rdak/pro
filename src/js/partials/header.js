1048ec9b966fab4b1137a15e17c0964f99970bef
rdak <seva@chebykin.com>
2016-06-22 03:32:31 +0600

    add conversations

--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
+	$( window ).scroll(function() {
+		if (window.pageXOffset){
+			$('body').addClass('js-scroll');
+		}
+	});
 });
 
-$( window ).scroll(function() {
-	if (window.pageXOffset){
-		$('body').addClass('js-scroll');
-	}
-});
 
