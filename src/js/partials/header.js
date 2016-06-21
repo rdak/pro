7e844699d113fb21819c7d5425dbeb7814611a73
rdak <seva@chebykin.com>
2016-06-22 04:16:45 +0600

    add footer/fix smht

--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 	$( window ).scroll(function() {
-		if (window.pageXOffset){
+		if (window.pageYOffset > 0){
+			console.log(window.pageYOffset);
 			$('body').addClass('js-scroll');
+		}
+		else{
+			$('body').removeClass('js-scroll');	
 		}
 	});
 });
