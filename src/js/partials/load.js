37cd9344f4e6378b6a766a2ca633c8c1ed41e34f
rdak <seva@chebykin.com>
2016-07-17 18:56:48 +0600

    fix / add pages

--- src/js/partials/load.js
+++ src/js/partials/load.js
@@ -1,11 +1,27 @@
-$(document).ready(function () {
+$(document).ready(function() {
 	loader();
+	$('.js-search').on('click', function(e){
+		e.preventDefault();
+		$(this).toggleClass('active');
+		if ($(this).hasClass('active')){
+			var top = 0;
+		}
+		else{
+			var top = -140;
+		}
+
+		$('.searchText-header').animate({
+			top: top
+		}, 500, function(){
+			$(this).find('input').focus();
+		});
+	});
 });
 
 var loader = function(){
 	$('.loader-wrapper').animate({
 		opacity: 0
-	}, 3000, function(){
+	}, 1000, function(){
 		$('body').css({
 			'overflow' : 'visible'
 		});
